9128d31a72929718e789249ead5f1092114d23bb
nabilaafifahqotrunnada <nabila_afifah_28rpl@student.smktelkom-mlg.sch.id>
2020-07-25 08:29:23 +0700

    Second Commit

---
+++ 3.js
@@ -0,0 +1,19 @@
+let siswa1 = {
+    nis: "101",
+    nama: "Suneo",
+    jurusan: "RPL"
+}
+let siswa2 = {
+    nis: "102",
+    nama: "Jeko",
+    jurusan: "TKJ"
+}
+
+//tampil nama siswa1
+console.log("Nama siswa 1 = " + siswa1.nama)
+
+//ubah nama siswa1
+siswa1.nama = "Nobita"
+
+//tampil nama siswa1 setelah diubah
+console.log("Nama siswa 1 = " + siswa1.nama)
